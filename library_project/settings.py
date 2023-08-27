c509f362ca544d52ef499fe6010640dc32b57ef7
Zeinab <zeinabfarhat38@gmail.com>
2023-08-27 11:18:23 +0200

    Fixed update book display

--- library_project/settings.py
+++ library_project/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login'
